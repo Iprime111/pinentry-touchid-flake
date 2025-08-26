8157fefc23e4fc46339081c666f8063a2e556a6f
Maxim Timoshkin <maximatimoshkin@outlook.com>
2025-08-26 13:38:57 +0300

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Пакет для бинарного файла из GitHub";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "aarch64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
+      version = "v0.0.3";
+      sha256 = "0wkppmlq4795xpikmcjalsmy12d9519ph0p4x9gf8v435sh2873g"; # Замените на актуальный хэш
+    in {
+      packages.${system} = {
+        pinentry-touchid = pkgs.stdenv.mkDerivation {
+          pname = "pinentry-touchid";
+          inherit version;
+
+          src = pkgs.fetchurl {
+            url = "https://github.com/jorgelbg/pinentry-touchid/releases/download/${version}/pinentry-touchid_0.0.3_macos_arm64.tar.gz";
+            inherit sha256;
+          };
+
+          # Определите source root если нужно
+          sourceRoot = ".";
+
+          installPhase = ''
+            install -D -m755 pinentry-touchid_0.0.3_macos_arm64/pinentry-touchid $out/bin/pinentry-touchid
+          '';
+
+          meta = with pkgs.lib; {
+            description = "TouchId version of pinentry-mac";
+            homepage = "https://github.com/jorgelbg/pinentry-touchid";
+            license = licenses.asl20;
+            platforms = [ "aarch64-darwin" ];
+          };
+        };
+
+        default = self.packages.${system}.pinentry-touchid;
+      };
+    };
+}
